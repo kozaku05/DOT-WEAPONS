57bd944102fca48a025c2dcf227dc8de7405c416
kozaku05 <kozaku0505@gmail.com>
2024-10-19 23:39:52 +0900

    いろいろ追加

--- sctipt.js
+++ sctipt.js
@@ -1,24 +1,25 @@
 const explanation = document.getElementById("explanation");
 explanation.style.display = "none";
-const storage =localStorage;
+const storage = localStorage;
 // 初めてサイトに入った時
-let history = storage.getItem('history');
 let name = storage.getItem('name');
-if(!name){
-    let navigation = document.getElementById("navigation");
-    navigation.style.display ="none";
-    storage.setItem('history' , 1);
-    let historyCount = document.getElementById("historyCount");
-    historyCount.innerHTML = '初めまして！まずはチュートリアルをお楽しみください！'
+let navigation = document.getElementById("navigation");
+if (!name) {
+    navigation.style.display = "none";
     explanation.style.display = "block";
-}else{
-    storage.setItem('history' , parseInt(history)+1);
-    let historyCount = document.getElementById("historyCount");
-    historyCount.innerHTML = `訪問回数は${parseInt(history)+1}回目です`
-};
+} else {
+        let weaponsCount = storage.getItem("weaponsCount");
+        if (!weaponsCount) {
+            let info = document.getElementById("info");
+            info.innerHTML = "最初の武器を獲得しました:こん棒";
+            storage.setItem('weaponsCount', 1);
+            storage.setItem("id1", 10);
+        };
+}
 
-function registername(){
+
+function registername() {
     let name = document.getElementById("name").value;
-    storage.setItem('name',name); 
+    storage.setItem('name', name);
     location.reload();
 }
