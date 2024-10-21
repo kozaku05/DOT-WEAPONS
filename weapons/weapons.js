a886253946fc4c33a54df4a82bb02b08f012fb86
kozaku05 <kozaku0505@gmail.com>
2024-10-22 06:39:13 +0900

    small fix by junsuzu

--- weapons/weapons.js
+++ weapons/weapons.js
@@ -1,26 +1,24 @@
 // 名前が登録されてなかったときページを戻す
-function backPage(){
-    window.location.href = '../index.html';
+function backPage() {
+  window.location.href = "/";
 }
-localStorage.getItem('name')||backPage()
-let money = localStorage.getItem('money');
-document.getElementById('money').innerHTML=`あなたの所持金は：${money}マネー`;
-
+localStorage.getItem("name") || backPage();
+let money = localStorage.getItem("money");
+document.getElementById("money").innerHTML = `あなたの所持金は：${money}マネー`;
+let weapons = fetch("/weapon.json").then((response) => response.json());
+console.log(weapons);
 //ガチャのコード
-function type1(){
-    if(money<800){
-
-    }
+function type1() {
+  if (money < 800) {
+  }
 }
 
-function type2(){
-    if(money<1000){
-
-    }
+function type2() {
+  if (money < 1000) {
+  }
 }
 
-function type3(){
-    if(money<1000){
-
-    }
+function type3() {
+  if (money < 1000) {
+  }
 }
